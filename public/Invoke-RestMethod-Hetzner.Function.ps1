42cfdb95fe2815934e31130b55552cf3072d4371
manuel-sperrle-stb-de <manuel.sperrle@stb.de>
2023-07-19 14:12:46 +0200

    cleaned: minor name changes / comments added

--- public/Invoke-RestMethod-Hetzner.Function.ps1
+++ public/Invoke-RestMethod-Hetzner.Function.ps1
@@ -1,37 +1,47 @@
-# https://dns.hetzner.com/api-docs/
 Function Invoke-RestMethod-Hetzner {
+# https://dns.hetzner.com/api-docs/
 
     [CmdletBinding()]
     param (     
 
+        # The query itself - example: 'zones'
         [Parameter(Mandatory, ValueFromPipeline)]
         [string[]]
         $Query,
         
+        # The Base Uri/URL used - example: 'https://dns.hetzner.com/api/v1'
         [Parameter(Mandatory)]
         [string]
         $BaseUri,
 
+        # The AuthAPIToken used - example: '12345678901234567890123456789012'
         [Parameter(Mandatory)]
         [string]
         $AuthAPIToken,
 
+        # -eq Invoke-RestMethod
         [string]
         $ContentType = 'application/json',
 
+        # -eq Invoke-RestMethod
         [string]
-        $Method = 'GET'
+        $Method = 'GET',
+
+        # -eq Invoke-RestMethod
+        [object]
+        $Body
 
     )
 
     begin {
 
         $RestMethodParams = @{
-            Method      = $Method            
-            Headers     = @{
+            Method  = $Method            
+            Headers = @{
                 'Auth-API-Token' = $AuthAPIToken
                 'Content-Type'   = $ContentType
             }
+            Body    = $Body
         }
 
     }
@@ -40,10 +50,7 @@ Function Invoke-RestMethod-Hetzner {
 
         $Query | ForEach-Object {
 
-            #[Collections.Generic.List[object]]$Return = @()
-
             $RestMethodParams.Uri = '{0}/{1}' -f $BaseUri.TrimEnd('/'), $Query.TrimStart('/')
-            # API will Error if pages is too high        
             
             $Page = 1
             do {
@@ -52,12 +59,13 @@ Function Invoke-RestMethod-Hetzner {
                 $ThisRestMethodParams.Uri = $ThisRestMethodParams.Uri + "?page=$Page"
 
                 try {
+                    # API will Error if pages is too high
                     $Answer = Invoke-RestMethod @ThisRestMethodParams -ErrorAction Stop
                 }
                 catch {
                     # tbd: catch/handle errors
                     $Finished = $true
-                    $Answer = $false                    
+                    $Answer = $false
                 }
                 finally {
                     if (-not $Answer) { $Finished = $true }
